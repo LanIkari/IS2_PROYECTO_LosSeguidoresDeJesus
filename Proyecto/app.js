1effee7ae6aaeb84cc63dd442209e38e6a82d0e9
Juan Carlos Perez Angeles <juan-carlos-perez-@outlook.com>
2022-05-23 13:32:24 -0500

    Corrección app.js

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -1,15 +1,11 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://Ikari:<password>@cluster0.ovphw.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true});
+
 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var peliculasRouter = require('./routes/peliculas');
+
 
 var app = express();
 
@@ -17,15 +13,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/peliculas', peliculasRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
