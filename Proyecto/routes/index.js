4be2ade1af4d8c02faef9aa3c6f089e69e72a21b
Eduardo Mendieta <mendieta.eduardo72@gmail.com>
2022-05-23 22:08:38 -0500

    Creacion de Metodos PUT y PATCH

--- Proyecto/routes/index.js
+++ Proyecto/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const Peliculas = require('../config/esquemas');
 require('../config/conexion');
-
+//METODO GET
 router.get('/peliculas', (req, res) => {
     Peliculas.find().then(result => {
         res.status(200).json({
@@ -10,7 +10,7 @@ router.get('/peliculas', (req, res) => {
         });
     });
 });
-
+//METODO GET POR ID
 router.get('/peliculas/:id', (req, res) => {
     Peliculas.findOne({'_id': req.params.id}).then(result => {
         res.status(200).json({
@@ -18,7 +18,7 @@ router.get('/peliculas/:id', (req, res) => {
         });
     });
 });
-
+//METODO POST
 router.post('/peliculas', (req, res, next) => {
     const pelicula = new Peliculas({
         _id: req.body._id,
@@ -35,38 +35,98 @@ router.post('/peliculas', (req, res, next) => {
         message: 'Producto Agregado'
     });
 });
+//METODO PUT
+router.put('/peliculas/:id', (req, res) => {
+    Peliculas.findByIdAndUpdate({'_id': req.params.id}, {
+        id: req.params.id,
+        titulo: req.body.titulo,
+        anno: req.body.anno,
+        genero: req.body.genero,
+        duracion: req.body.duracion,
+        sinopsis: req.body.sinopsis,
+        director: req.body.director,
+        reparto: req.body.reparto
+    }, function (err, result) {
+        if (err) {
+            console.log(err)
+        }
+        if (!result) {
+            res.status(404).json({message: "Pelicula no encontrada"})
+            var pelicula = Peliculas({
+                _id: req.params.id,
+                titulo: req.body.titulo,
+                anno: req.body.anno,
+                genero: req.body.genero,
+                duracion: req.body.duracion,
+                sinopsis: req.body.sinopsis,
+                director: req.body.director,
+                reparto: req.body.reparto
+            });
+            pelicula.save();
+        } else {
+            res.status(200).json({
+                message: 'Producto actualizado'
+            })
+        }
+    });
+});
+//METODO PATCH
+router.patch('/peliculas/:id', (req, res) => {
+    Peliculas.findByIdAndUpdate({'_id': req.params.id}, {
+        id: req.params.id,
+        titulo: req.body.titulo,
+        anno: req.body.anno,
+        genero: req.body.genero,
+        duracion: req.body.duracion,
+        sinopsis: req.body.sinopsis,
+        director: req.body.director,
+        reparto: req.body.reparto
+    }, function (err, result) {
+        if (err) {
+            console.log(err)
+        }
+        if (!result) {
+            res.status(404).json({message: "Pelicula no encontrada"})
+        } else {
+            res.status(200).json({
+                message: 'Producto actualizado'
+            })
+        }
 
+    });
+});
+//METODO DELETE
 router.delete('/peliculas/:id', (req, res, next) => {
     Peliculas.deleteOne({'_id': req.params.id}).then(result => {
         res.status(200).json({message: "Producto eliminado"});
     });
 });
 
-router.get('/',(req,res,next) =>{
+router.get('/', (req, res, next) => {
     res.render('desarrollador')
 })
 
-router.get('/archiDocker',(req,res,next) =>{
+router.get('/archiDocker', (req, res, next) => {
     res.render('archiDocker')
 })
 
-router.get('/contenedor',(req,res,next) =>{
+router.get('/contenedor', (req, res, next) => {
     res.render('contenedor')
 })
 
-router.get('/direcDocker',(req,res,next) =>{
+router.get('/direcDocker', (req, res, next) => {
     res.render('direcDocker')
 })
 
-router.get('/imgDocker',(req,res,next) =>{
+router.get('/imgDocker', (req, res, next) => {
     res.render('imgDocker')
 })
 
-router.get('/introDocker',(req,res,next) =>{
+router.get('/introDocker', (req, res, next) => {
     res.render('introDocker')
 })
 
-router.get('/vidaContenedor',(req,res,next) =>{
+router.get('/vidaContenedor', (req, res, next) => {
     res.render('vidaContenedor')
 })
 
